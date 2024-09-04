565424757093b0693465f36454905f13723b0406
andesrule <thomasdp420@gmail.com>
2024-09-04 15:15:07 +0200

    changes in backend structure

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class MyString(db.Model):
+    __tablename__ = 'my_strings'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
