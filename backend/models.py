53616f5e7282b55621d26591cc792de61bb16e21
RatChef24 <61520192+RatChef24@users.noreply.github.com>
2024-09-30 17:45:56 +0200

    Reapply "Merge branch 'task_6' of https://github.com/andesrule/ContextAware2024 into task_6"
    This reverts commit 477c404724dc390465287420bb0a517d95cfa457.

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
 from sqlalchemy.dialects.postgresql import ARRAY 
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -11,13 +13,12 @@ class User(db.Model):
     
 class Geofence(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    markers = db.Column(JSON)  # Memorizza i marker come JSON
-    geofences = db.Column(JSON)  # Memorizza i poligoni come JSON
+    marker = db.Column(Geometry(geometry_type='POINT', srid=4326))  # Memorizza i marker come POINT
+    geofence = db.Column(Geometry(geometry_type='POLYGON', srid=4326))  # Memorizza i poligoni come POLYGON
 
-    def __init__(self, markers=None, geofences=None):
-        self.markers = markers or []
-        self.geofences = geofences or []
-
+    def __init__(self, marker=None, geofence=None):
+        self.marker = marker
+        self.geofence = geofence
 
 class QuestionnaireResponse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,3 +32,8 @@ class QuestionnaireResponse(db.Model):
     farmacia = db.Column(db.Integer)
     luogo_culto = db.Column(db.Integer)
     servizi = db.Column(db.Integer)
+    densita_aree_verdi = db.Column(db.Integer)
+    densita_cinema = db.Column(db.Integer)
+    densita_fermate_bus = db.Column(db.Integer)
+
+
